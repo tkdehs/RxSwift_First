daf2c1044db3df41c4eafbc2b7f21ef1e6469166
SangDon <tkdehs0454@naver.com>
2022-05-07 23:41:08 +0900

    기본기능 테스트
    기본기능 테스트

--- Hellow/Hellow.playground/Contents.swift
+++ Hellow/Hellow.playground/Contents.swift
@@ -3,6 +3,21 @@ import RxSwift
 
 let disposeBag = DisposeBag()
 
-Observable.just("Hello, RxSiwft")
-    .subscribe { print($0) }
+//Observable.just("Hello, RxSiwft")
+//    .subscribe { print($0) }
+//    .disposed(by: disposeBag)
+
+//var a = 1
+//var b = 2
+//a + b
+//
+//a = 12
+
+let a = BehaviorSubject(value: 1)
+let b = BehaviorSubject(value: 2)
+
+Observable.combineLatest(a, b) { $0 + $1 }
+    .subscribe(onNext: {print($0) })
     .disposed(by: disposeBag)
+
+a.onNext(12)
